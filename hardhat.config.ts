2135f1dfa22510b060aec6bed414366f2b4f4aa9
Shivansh <shivansh0301@gmail.com>
2024-10-25 10:21:25 +0530

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -150,8 +150,34 @@ const userConfig: HardhatUserConfig = {
             zksync: true,
             verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
         },
+        'sepolia-titan': {
+            url: `${process.env.NODE_URL}`,
+            accounts: [`${process.env.PK}`],
+            chainId: 55007,
+            gasPrice: 1,
+        },
     },
-    deterministicDeployment,
+    // deterministicDeployment,
+    deterministicDeployment: (network: string) => {
+        // Skip on hardhat's local network.
+        if (network === "31337") {
+            return undefined;
+        } else if (network === "55007") {
+            return {
+                factory: "0x97A23639dbce0507Ee466741AAb1A6BD4EB7a38c",
+                deployer: "0x08a74A0075a2C3A786A84439812a141C6C8b73f3",
+                funding: "1000000000000000",
+                signedTx: "0x00",
+            };
+        } else {
+            return {
+                factory: "0x4e59b44847b379578588920ca78fbf26c0b4956c",
+                deployer: "0x3fab184622dc19b6109349b94811493bf2a45362",
+                funding: "10000000000000000",
+                signedTx: "0x00",
+            };
+        }
+    },
     namedAccounts: {
         deployer: 0,
     },
@@ -159,7 +185,21 @@ const userConfig: HardhatUserConfig = {
         timeout: 2000000,
     },
     etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+        apiKey: {
+            goerli: `${process.env.ETHERSCAN_API_KEY}`,
+            sepolia: `${process.env.ETHERSCAN_API_KEY}`,
+            titansepolia: "verify",
+        },
+        customChains: [
+            {
+                network: "sepolia-titan",
+                chainId: 55007,
+                urls: {
+                    apiURL: "https://explorer.titan-sepolia.tokamak.network/api",
+                    browserURL: "https://explorer.titan-sepolia.tokamak.network",
+                },
+            },
+        ],
     },
 };
 if (NODE_URL) {
